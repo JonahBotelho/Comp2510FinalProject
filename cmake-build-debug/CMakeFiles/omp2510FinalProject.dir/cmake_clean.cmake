34ddd8dfd850bf970f7e030849e6058125fac4ee
Cole Campbell <ccampbell211@my.bcit.ca>
2025-02-06 14:45:41 -0800

    Added main method for main menu and created struct Patient

---
+++ cmake-build-debug/CMakeFiles/omp2510FinalProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/omp2510FinalProject.dir/hospital_management.c.o"
+  "CMakeFiles/omp2510FinalProject.dir/hospital_management.c.o.d"
+  "omp2510FinalProject"
+  "omp2510FinalProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/omp2510FinalProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
