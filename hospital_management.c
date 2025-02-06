64d23f7417ab5236d3eeaf0df7b1ad6c54ba5e3a
Jonah Botelho <jbotelho3@my.bcit.ca>
2025-02-05 22:35:27 -0800

    Changed Patient integers to unsigned

--- hospital_management.c
+++ hospital_management.c
@@ -15,11 +15,11 @@
 // Define the structure for a Patient record
 typedef struct
 {
-    int patientID; // Unique Patient ID
+    unsigned int patientID; // Unique Patient ID
     char name[MAX_CHARS_IN_NAME]; // Full Name of the patient
-    int age; // Age of the patient
+    unsigned int age; // Age of the patient
     char diagnosis[MAX_CHARS_IN_DIAGNOSIS]; // Medical condition or reason for admission
-    int roomNumber; // Assigned room number in the hospital
+    unsigned int roomNumber; // Assigned room number in the hospital
 } Patient;
 
 int addPatient(Patient patients[], int *patientCount);
